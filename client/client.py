32f57f6024bf5b6f64cf970c57c1e4f8bf585713
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-15 12:33:45 -0400

    set up client further

--- client/client.py
+++ client/client.py
@@ -1,5 +1,18 @@
 import os
+import sys
+sys.path.insert(0, "../")
+import config
 import boto3
 
-sns = boto3.resource('sns')
-subscription = sns.Subscription('arn')
+client = boto3.client(
+    'sns', 
+    region_name=config.aws_default_region,
+    aws_access_key_id=config.aws_access_key_id,
+    aws_secret_access_key=config.aws_secret_access_key
+)
+
+client.subscribe(
+    TopicArn=config.sns_arn,
+    Protocol='sqs',
+    Endpoint=config.sqs_arn
+)
