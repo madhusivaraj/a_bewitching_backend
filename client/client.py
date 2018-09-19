813183b232eff7600888b8cae1d08b5a9bcf9c63
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-19 00:38:10 -0400

    cleared up printing

--- client/client.py
+++ client/client.py
@@ -4,6 +4,8 @@ sys.path.insert(0, "../")
 import config
 import boto3
 
+CLIENT_WAIT_TIME=20
+
 # Create SQS client
 client = client = boto3.client(
     'sqs', 
@@ -22,15 +24,19 @@ while(True):
         MessageAttributeNames=[
             'All'
         ],
-        WaitTimeSeconds=20
+        WaitTimeSeconds=CLIENT_WAIT_TIME
     )
-    print(response, end="\n\n")
     
     if 'Messages' in response:
         message = response['Messages'][0]
+        body = message['Body']
         receipt_handle = message['ReceiptHandle']
-        
+
+        print(body, end="\n\n")
+
         client.delete_message(
             QueueUrl=config.sqs_url,
             ReceiptHandle=receipt_handle
         )
+    else:
+        print(response, end="\n\n")
