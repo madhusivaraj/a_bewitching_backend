215c775a43f6c02060f1b7f044b7ae5317bc4196
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-19 00:32:35 -0400

    got client to listen properly

--- client/client.py
+++ client/client.py
@@ -4,15 +4,33 @@ sys.path.insert(0, "../")
 import config
 import boto3
 
-client = boto3.client(
-    'sns', 
+# Create SQS client
+client = client = boto3.client(
+    'sqs', 
     region_name=config.aws_default_region,
     aws_access_key_id=config.aws_access_key_id,
     aws_secret_access_key=config.aws_secret_access_key
 )
 
-client.subscribe(
-    TopicArn=config.sns_arn,
-    Protocol='sqs',
-    Endpoint=config.sqs_arn
-)
+while(True):
+    response = client.receive_message(
+        QueueUrl=config.sqs_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        WaitTimeSeconds=20
+    )
+    print(response, end="\n\n")
+    
+    if 'Messages' in response:
+        message = response['Messages'][0]
+        receipt_handle = message['ReceiptHandle']
+        
+        client.delete_message(
+            QueueUrl=config.sqs_url,
+            ReceiptHandle=receipt_handle
+        )
