e0a6073f6bcc6e91ec794f13ee149db3b85776bd
Srihari <srihari.shankar1@gmail.com>
2018-10-15 21:59:22 +0000

    use aws creds from aws configure

--- client/client.py
+++ client/client.py
@@ -4,16 +4,13 @@ import glog as log
 sys.path.insert(0, "../")
 import config
 import boto3
+from gpiozero import LED
+led = LED(17)
 
 CLIENT_WAIT_TIME=20
 
 # Create SQS client
-client = client = boto3.client(
-    'sqs', 
-    region_name=config.aws_default_region,
-    aws_access_key_id=config.aws_access_key_id,
-    aws_secret_access_key=config.aws_secret_access_key
-)
+client = boto3.client('sqs')
 
 while(True):
     response = client.receive_message(
@@ -34,10 +31,11 @@ while(True):
         receipt_handle = message['ReceiptHandle']
 
         print(body, end="\n\n")
-
+        led.on()
         client.delete_message(
             QueueUrl=config.sqs_url,
             ReceiptHandle=receipt_handle
         )
     else:
-        print(response, end="\n\n")
+#        print(response, end="\n\n")
+         pass
