381964f88ebd83fe5976215924bdd05a0701fb51
Srihari Shankar <srihari.shankar1@gmail.com>
2018-10-24 23:47:03 +0000

    Updated audio files

--- media/play_sound.py
+++ media/play_sound.py
@@ -4,12 +4,17 @@
 # so any sounds / video that get played will play their entire length
 
 import os
+import time
 import subprocess
 
-f1 = os.getcwd() + '/terror.mp3'
-f2 = os.getcwd() + '/walking_sound.mp3'
-
-do = subprocess.Popen(['afplay', f1])
-re = subprocess.Popen(['afplay', f2])
+f1 = 'static.mp4'
+f2 =  'static_sound.mp3'
 
 
+do = subprocess.Popen(['omxplayer', '-b', f1, '&'])
+re = subprocess.Popen(['omxplayer',  '-o',  'local', f2, '&'])
+
+time.sleep(10)
+
+do.kill()
+re.kill()
--- media/trigger_audio.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-file = sys.argv[1]
-os.system("omxplayer --display=5 " + file)
-
