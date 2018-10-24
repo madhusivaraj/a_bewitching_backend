8cfac6372564fc93af923424887f269f7ccfd99d
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-10-24 17:50:46 -0400

    cleaned up background process

--- client/rsp_2.py
+++ client/rsp_2.py
@@ -8,6 +8,9 @@ silhouette_pin = 23 #orange wire
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(silhouette_pin, GPIO.OUT)
 
+silhouette_sound = '../media/footsteps.mp3'
+
+
 def silhouette_pin_on():
 	GPIO.output(silhouette_pin, True)
 
@@ -15,8 +18,19 @@ def silhouette_pin_off():
 	GPIO.output(silhouette_pin, False)
 
 def silhouette_event():
-	silhouette_sound = '../media/footsteps.mp3'
+	subprocess.Popen(['omxplayer', silhouette_sound, '&'])
+	
 	silhouette_pin_on()
-	subprocess.Popen(['omxplayer', silhouette_sound])
 	time.sleep(2)
 	silhouette_pin_off()
+
+def cleanup():
+	GPIO.cleanup()
+
+try:
+	silhouette_pin_off()
+	while True:
+		silhouette_event()
+		time.sleep(5)
+except KeyboardInterrupt:
+	cleanup()
